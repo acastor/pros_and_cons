21f95389a70b2b466aa8db956deb4bbeb0835085
acastor <6983427+acastor@users.noreply.github.com>
2019-11-16 20:12:08 -0600

    customizing 404 page

--- gatsby-site/src/pages/404.js
+++ gatsby-site/src/pages/404.js
@@ -1,13 +1,14 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HomeAloneImg from "../images/home_alone.gif"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img src={HomeAloneImg} alt="home alone scream" />
+    <h1>WOAH WOAH WOAH</h1>
+    <p>You just hit a nonexistent route, get outta here ya filthy animal!</p>
   </Layout>
 )
 
