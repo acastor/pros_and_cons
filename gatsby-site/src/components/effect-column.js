1571154b5245d7a95d24e6f0641906773a23bf82
acastor <6983427+acastor@users.noreply.github.com>
2019-11-16 23:47:44 -0600

    more styling changes and cleanup

--- gatsby-site/src/components/effect-column.js
+++ gatsby-site/src/components/effect-column.js
@@ -2,12 +2,17 @@ import React from "react"
 import EffectList from "./list"
 import ListForm from "./list-form"
 import useEffectState from "./useEffectState"
+import styled from "styled-components"
+
+const Title = styled.h1`
+  text-align: center;
+`
 
 const EffectColumn = ({ placeholder, title }) => {
   const { effects, saveEffect, deleteEffectAtIndex } = useEffectState([])
   return (
     <div>
-      <h1>{title}</h1>
+      <Title>{title}</Title>
       <div>
         <ListForm
           placeholder={placeholder}
--- gatsby-site/src/pages/404.js
+++ gatsby-site/src/pages/404.js
@@ -2,14 +2,32 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HomeAloneImg from "../images/home_alone.gif"
+import Header from "../components/header"
+import { useStaticQuery, graphql } from "gatsby"
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <img src={HomeAloneImg} alt="home alone scream" />
-    <h1>WOAH WOAH WOAH</h1>
-    <p>You just hit a nonexistent route, get outta here ya filthy animal!</p>
-  </Layout>
-)
+  return (
+    <div>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <Layout>
+        <SEO title="404: Not found" />
+        <img src={HomeAloneImg} alt="home alone scream" />
+        <h1>WOAH WOAH WOAH</h1>
+        <p>
+          You just hit a nonexistent route, get outta here ya filthy animal!
+        </p>
+      </Layout>
+    </div>
+  )
+}
 
 export default NotFoundPage
